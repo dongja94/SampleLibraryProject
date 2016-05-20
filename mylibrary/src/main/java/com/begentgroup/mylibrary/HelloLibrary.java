d3d52e823b531796a6ad58bf84b3772b30ab6044
dongja94 <dongja94@gmail.com>
2016-05-20 09:18:10 +0900

    Initial commit

---
+++ mylibrary/src/main/java/com/begentgroup/mylibrary/HelloLibrary.java
@@ -0,0 +1,10 @@
+package com.begentgroup.mylibrary;
+
+/**
+ * Created by dongja94 on 2016-05-16.
+ */
+public class HelloLibrary {
+    public String getMessage() {
+        return "Hello Library";
+    }
+}
